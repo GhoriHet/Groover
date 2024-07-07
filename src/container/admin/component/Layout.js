8c374ada71962b83448de5cde30c70e527da1a8a
GhoriHet <ghorihet71@gmail.com>
2024-07-07 11:31:49 +0530

    Okay

--- src/container/admin/component/Layout.js
+++ src/container/admin/component/Layout.js
@@ -47,15 +47,6 @@ function Layout({ children }) {
                             <i class="uil uil-signout"></i>
                             <span class="link-name">Logout</span>
                         </a></li>
-                        <li class="mode">
-                            <a href="#">
-                                {/* <i class="uil uil-moon"></i> */}
-                                {/* <span class="link-name">Dark Mode</span> */}
-                            </a>
-                            <div class="mode-toggle">
-                                {/* <span class="switch"></span> */}
-                            </div>
-                        </li>
                     </ul>
                 </div>
             </div>
