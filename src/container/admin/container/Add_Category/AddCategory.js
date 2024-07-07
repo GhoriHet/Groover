2bfdf007f24bbda9157e5145a2f45a25dfa4ea7e
GhoriHet <ghorihet71@gmail.com>
2024-07-07 12:32:37 +0530

    done

---
+++ src/container/admin/container/Add_Category/AddCategory.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import AddIcon from '@mui/icons-material/Add';
+
+function AddCategory(props) {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false)
+        // formik.resetForm()
+        // setUpdate(null);
+    }
+
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'firstName', headerName: 'First name', width: 130 },
+        { field: 'lastName', headerName: 'Last name', width: 130 },
+        {
+            field: 'age',
+            headerName: 'Age',
+            type: 'number',
+            width: 90,
+        },
+        {
+            field: 'fullName',
+            headerName: 'Full name',
+            description: 'This column has a value getter and is not sortable.',
+            sortable: false,
+            width: 160,
+            valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+        },
+    ];
+
+    const rows = [
+        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    ];
+    return (
+        <>
+            <div className='d-flex align-items-center justify-content-between' style={{ marginTop: '65px' }}>
+                <h3 className='mb-0' style={{ color: '#FF6337' }}>Add Category</h3>
+                <Button type="button" variant="contained" onClick={handleClickOpen}>Category <AddIcon fontSize="small" /></Button>
+            </div>
+            <Dialog id='addModal' open={open}>
+                <DialogTitle style={{ fontSize: '24px', fontWeight: 'bold', color: '#707070', fontFamily: 'Poppins' }} className='px-5 pt-4 pb-0 text-center'>Add Category</DialogTitle>
+                <DialogContent className='px-5 pb-4'>
+                    <form className='row' style={{ width: "500px", marginTop: '7px'}}>
+                        <div className="col-6 mb-3 form_field position-relative">
+                            <TextField className='m-0' margin="dense" id="cate_name" label="Category Name" type="text" fullWidth name='cate_name' variant="standard"
+                            // onChange={handleChange}
+                            // onBlur={handleBlur}
+                            // value={values.mediname}
+                            />
+                            {/* {errors.mediname && touched.mediname ? (
+                                <span className="d-block position-absolute form-error">{errors.mediname}</span>
+                            ) : null} */}
+                        </div>
+                        <div className="col-6 mb-3 form_field position-relative">
+                            <TextField className='m-0' margin="dense" id="cate_desc" label="Category Description" type="text" fullWidth name='cate_desc' variant="standard"
+                            // onChange={handleChange}
+                            // onBlur={handleBlur}
+                            // value={values.mediname}
+                            />
+                            {/* {errors.mediname && touched.mediname ? (
+                                <span className="d-block position-absolute form-error">{errors.mediname}</span>
+                            ) : null} */}
+                        </div>
+                        <div className='pt-3 col-12 text-center'>
+                            <Button className='me-3' onClick={handleClose}>Cancel</Button>
+                            <Button type="submit" variant="contained">Submit</Button>
+                        </div>
+                    </form>
+                </DialogContent>
+            </Dialog>
+            <div style={{ height: 400, width: '100%', marginTop: '50px' }}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: { page: 0, pageSize: 5 },
+                        },
+                    }}
+                    pageSizeOptions={[5, 10]}
+                />
+            </div>
+        </>
+    );
+}
+
+export default AddCategory;
