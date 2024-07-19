61c5e80d14a06c36ac25d34f8b1f0588124847b1
GhoriHet <ghorihet71@gmail.com>
2024-07-19 19:26:08 +0530

    ..

--- src/container/admin/container/Dashboard.js
+++ src/container/admin/container/Dashboard.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCategory } from '../../../Redux/Slice/AddCategory';
+import { fetchSubcategory } from '../../../Redux/Slice/AddSubcategorySlice';
+import { fetchProduct } from '../../../Redux/Slice/AddProductSlice';
 
 function Dashboard(props) {
+
+    const dispatch = useDispatch();
+
+    const catDataFetch = useSelector((state => state.category?.data));
+    const subcatDataFetch = useSelector((state => state.subcategory?.data));
+    const proDataFetch = useSelector((state => state.product?.data));
+
+    React.useEffect(() => {
+        dispatch(fetchCategory());
+        dispatch(fetchSubcategory());
+        dispatch(fetchProduct());
+    }, [dispatch])
+
     return (
         <div className="dash-content">
             <div className="overview">
@@ -13,78 +30,44 @@ function Dashboard(props) {
                     <div className="box box1">
                         {/* <i className="uil uil-thumbs-up" /> */}
                         <span className="text">Total Category</span>
-                        <span className="number">50,120</span>
+                        <span className="number">{catDataFetch?.data?.length}</span>
                     </div>
                     <div className="box box2">
                         {/* <i className="uil uil-comments" /> */}
                         <span className="text">Total Subcategory</span>
-                        <span className="number">20,120</span>
+                        <span className="number">{subcatDataFetch?.data?.length}</span>
                     </div>
                     <div className="box box3">
                         {/* <i className="uil uil-share" /> */}
                         <span className="text">Total Product</span>
-                        <span className="number">10,120</span>
+                        <span className="number">{proDataFetch?.data?.length}</span>
                     </div>
                 </div>
             </div>
-            {/* <div className="activity">
+            <div className="activity">
                 <div className="title">
-                    <i className="uil uil-clock-three" />
+                    <DashboardIcon style={{ marginRight: '18px', fontSize: '28px', color: '#707070' }} />
                     <span className="text">Recent Activity</span>
                 </div>
                 <div className="activity-data">
                     <div className="data names">
-                        <span className="data-title">Name</span>
-                        <span className="data-list">Prem Shahi</span>
-                        <span className="data-list">Deepa Chand</span>
-                        <span className="data-list">Manisha Chand</span>
-                        <span className="data-list">Pratima Shahi</span>
-                        <span className="data-list">Man Shahi</span>
-                        <span className="data-list">Ganesh Chand</span>
-                        <span className="data-list">Bikash Chand</span>
+                        <span className="data-title">Category Name</span>
+                        <span className="data-list">{catDataFetch?.data?.map((v) => v.category_name)}</span>
                     </div>
                     <div className="data email">
-                        <span className="data-title">Email</span>
-                        <span className="data-list">premshahi@gmail.com</span>
-                        <span className="data-list">deepachand@gmail.com</span>
-                        <span className="data-list">prakashhai@gmail.com</span>
-                        <span className="data-list">manishachand@gmail.com</span>
-                        <span className="data-list">pratimashhai@gmail.com</span>
-                        <span className="data-list">manshahi@gmail.com</span>
-                        <span className="data-list">ganeshchand@gmail.com</span>
+                        <span className="data-title">Subcategory Name</span>
+                        <span className="data-list">{subcatDataFetch?.data?.map((v) => v.subcategory_name)}</span>
                     </div>
                     <div className="data joined">
-                        <span className="data-title">Joined</span>
-                        <span className="data-list">2022-02-12</span>
-                        <span className="data-list">2022-02-12</span>
-                        <span className="data-list">2022-02-13</span>
-                        <span className="data-list">2022-02-13</span>
-                        <span className="data-list">2022-02-14</span>
-                        <span className="data-list">2022-02-14</span>
-                        <span className="data-list">2022-02-15</span>
+                        <span className="data-title">Product Name</span>
+                        <span className="data-list">{proDataFetch?.data?.map((v) => v.name)}</span>
                     </div>
                     <div className="data type">
-                        <span className="data-title">Type</span>
+                        <span className="data-title">Supplier</span>
                         <span className="data-list">New</span>
-                        <span className="data-list">Member</span>
-                        <span className="data-list">Member</span>
-                        <span className="data-list">New</span>
-                        <span className="data-list">Member</span>
-                        <span className="data-list">New</span>
-                        <span className="data-list">Member</span>
-                    </div>
-                    <div className="data status">
-                        <span className="data-title">Status</span>
-                        <span className="data-list">Liked</span>
-                        <span className="data-list">Liked</span>
-                        <span className="data-list">Liked</span>
-                        <span className="data-list">Liked</span>
-                        <span className="data-list">Liked</span>
-                        <span className="data-list">Liked</span>
-                        <span className="data-list">Liked</span>
                     </div>
                 </div>
-            </div> */}
+            </div>
         </div>
 
     );
